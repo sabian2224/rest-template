812810b9af859c67c3c46566279ed760bd05c097
Sabian Zhupa <szhupa20@epoka.edu.al>
2024-04-06 21:40:01 +0200

    header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
-
+import { images } from '../../constants';
 import './Header.css';
+import { SubHeading } from '../../components';
+
+
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">
+        The Key ti Fine Dining
+      </h1>
+
+      <p className="p__opensans" style={{margin: '2rem 0'}}>
+        This is just some text, This is just some text,This is just some text,This is just some text,
+      </p>
+
+      <button className="custom__button" type='button'>
+        Explore Menu
+      </button>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header img" />
+    </div>
+
+
   </div>
 );
 
