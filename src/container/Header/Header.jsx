60f3192e643ba2b070e0bb8621a70cb8eb688f42
Sabian Zhupa <szhupa20@epoka.edu.al>
2024-04-07 11:47:07 +0200

    Update Header.jsx

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => (
     <div className='app__wrapper_info'>
       <SubHeading title="Chase the new flavour" />
       <h1 className="app__header-h1">
-        The Key ti Fine Dining
+        The Key to Fine Dining
       </h1>
 
       <p className="p__opensans" style={{margin: '2rem 0'}}>
