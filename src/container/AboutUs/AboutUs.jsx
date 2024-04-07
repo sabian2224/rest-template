62675264868fe2dd3e069962a3c822ad899e6448
Sabian Zhupa <szhupa20@epoka.edu.al>
2024-04-07 14:33:21 +0200

    menu

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -3,7 +3,7 @@ import { images } from '../../constants';
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div className='app__aboutus app__bg flex__cente section__padding' id='about'> 
+  <div className='app__aboutus app__bg flex__center section__padding' id='about'> 
     <div className="app__aboutus-overlay flex__center">
       <img src={images.G} alt="g letter" />
     </div>
@@ -11,16 +11,28 @@ const AboutUs = () => (
     <div className="app__aboutus-content flex__center">
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">
-          Abous Us
+          About Us
         </h1>
-        <img src={images.spoon} alt="about_spoon" 
-        className='spoon__img'/>
-        <p className="p__opensans">
-          Here is a paragraph
-        </p>
+        <img src={images.spoon} alt="about_spoon" className='spoon__img'/>
+        <p className="p__opensans">Here is a paragraph</p>
         <button type='button' className='custom__button'>Know More</button>
-        
       </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src= {images.knife} alt="about_knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">
+          Our History
+        </h1>
+        <img src={images.spoon} alt="about_spoon" className='spoon__img'/>
+        <p className="p__opensans">Here is a paragraph</p>
+        <button type='button' className='custom__button'>Know More</button>
+      </div>
+
+
+
     </div>
   </div>
 );
